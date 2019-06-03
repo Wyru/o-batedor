1214ea62b7f5086b9b4ea460f62445796b10e0eb
Will Saymon <wsaymonoficial@gmail.com>
2019-06-03 12:34:35 -0300

    Add player movement

---
+++ Assets/Scripts/Cards/Data/Card.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+
+namespace Database
+{
+    [CreateAssetMenu(fileName = "Card Name", menuName = "Card/New Card", order = 0)]
+    public class Card : ScriptableObject {
+        public Sprite background;
+        public Sprite front;
+
+        public new string name;
+
+        public int rareLevel;
+        
+    }
+}
